2e9635de3985327cce9bb92f8a4ee61056bf03a2
Ariefsetiawan900 <arief.setiawan8788@gmail.com>
2024-09-13 18:00:44 +0700

    add cursor pointer in TextHeading

--- src/components/TextHeading.tsx
+++ src/components/TextHeading.tsx
@@ -1,4 +1,4 @@
-import {useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 
 interface TextHeadingProps {
   text: string | undefined;
@@ -7,7 +7,10 @@ interface TextHeadingProps {
 const TextHeading = ({ text }: TextHeadingProps) => {
   const navigate = useNavigate();
   return (
-    <h1 className="text-transparent bg-gradient-to-r from-ctp-blue to-ctp-white text-7xl font-bold bg-clip-text capitalize" onClick={()=>navigate('/')}>
+    <h1
+      className=" cursor-pointer text-transparent bg-gradient-to-r from-ctp-blue to-ctp-white text-7xl font-bold bg-clip-text capitalize"
+      onClick={() => navigate("/")}
+    >
       {text}
     </h1>
   );
