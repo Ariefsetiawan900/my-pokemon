7e625aa22beec9f3610a3c5f3148ac6730aeca84
Ariefsetiawan900 <arief.setiawan8788@gmail.com>
2024-09-13 17:56:45 +0700

    add routing in Text heading

--- src/components/TextHeading.tsx
+++ src/components/TextHeading.tsx
@@ -1,10 +1,13 @@
+import {useNavigate } from 'react-router-dom'
+
 interface TextHeadingProps {
   text: string | undefined;
 }
 
 const TextHeading = ({ text }: TextHeadingProps) => {
+  const navigate = useNavigate();
   return (
-    <h1 className="text-transparent bg-gradient-to-r from-ctp-blue to-ctp-white text-7xl font-bold bg-clip-text capitalize">
+    <h1 className="text-transparent bg-gradient-to-r from-ctp-blue to-ctp-white text-7xl font-bold bg-clip-text capitalize" onClick={()=>navigate('/')}>
       {text}
     </h1>
   );
